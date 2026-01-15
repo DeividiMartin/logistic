1d83b4c03c28f80619b70ea35d9a46ffc0d68e59
Deividi Martin <deividi_97@hotmail.com>
2026-01-15 18:45:22 -0300

    converte projeto para Spring Boot e expõe endpoints Rest de entrega

--- src/main/java/br/com/deividi/controller/EntregaController.java
+++ src/main/java/br/com/deividi/controller/EntregaController.java
@@ -6,7 +6,10 @@ import br.com.deividi.domain.Cliente;
 import br.com.deividi.domain.Endereco;
 import br.com.deividi.domain.Entrega;
 import br.com.deividi.service.EntregaService;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/entregas")
 public class EntregaController {
 
     private final EntregaService entregaService;
@@ -15,7 +18,8 @@ public class EntregaController {
         this.entregaService = entregaService;
     }
 
-    public Entrega criarEntrega(CriarEntregaRequest request) {
+    @PostMapping
+    public Entrega criarEntrega(@RequestBody CriarEntregaRequest request) {
         EnderecoRequest enderecoRequest = request.getEndereco();
         Cliente cliente = new Cliente(
                 request.getNomeCliente(),
@@ -31,18 +35,22 @@ public class EntregaController {
         return entregaService.criarEntrega(cliente, endereco);
     }
 
+    @GetMapping("/{id}")
     public Entrega buscarEntrega(Long id) {
         return entregaService.buscarEntrega(id);
     }
 
+    @PostMapping("/{id}/iniciar")
     public void iniciarTransporte(Long entregaId) {
         entregaService.iniciarTransporte(entregaId);
     }
 
+    @PostMapping("/{id}/finalizar")
     public void finalizarEntrega(Long entregaId) {
         entregaService.finalizarEntrega(entregaId);
     }
 
+    @PostMapping("/{id}/cancelar")
     public void cancelarEntrega(Long entregaId) {
         entregaService.cancelarEntrega(entregaId);
     }
