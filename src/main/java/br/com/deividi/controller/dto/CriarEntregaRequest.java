1684f5958c536776a18d4ca43931bcc171b87929
Deividi Martin <deividi_97@hotmail.com>
2026-02-13 14:03:57 -0300

    refactor: Standardize CPF handling and strengthen input validation

--- src/main/java/br/com/deividi/controller/dto/CriarEntregaRequest.java
+++ src/main/java/br/com/deividi/controller/dto/CriarEntregaRequest.java
@@ -3,13 +3,17 @@ package br.com.deividi.controller.dto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CPF;
 
 public class CriarEntregaRequest {
 
     @NotBlank(message = "Nome do cliente é obrigatório")
+    @Size (min = 3, max = 20, message = "Nome deve conter entre 3 e 20 caracteres")
     private String nomeCliente;
 
     @NotNull(message = "CPF é obrigatório")
+    @CPF (message = "CPF não é valido")
     private Long cpf;
 
     @NotNull(message = "Endereço é obrigatório")
